a181648b365426353623b32ed2d0e935c34656ee
MartinPettersen <searchingtheskyfor@gmail.com>
2024-10-25 14:17:42 +0200

    vue ecommerce-main-product-page setup

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts" >
+import { ref } from 'vue'
+      const name = ref("John Doe");
+      const shoppingCart = ref([]);
+      const productCount = ref(0);
+
+
+  const addProduct = () => {
+    productCount = 0;
+  }
+
+</script>
+
+<template>
+  <h1>Shoes {{ name }}</h1>
+  <span>
+    <ul>
+      <li v-for="product in shoppingCart" :key="product"> {{ product }}</li>
+    </ul>
+  </span>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
