a554791733c48711d877835211de2780891c955f
MartinPettersen <searchingtheskyfor@gmail.com>
2024-10-30 08:56:59 +0100

    fix layout menu mobile

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -1,11 +1,12 @@
 <script lang="ts" setup>
 import logo from '@/assets/logo.svg'
+import menu from '@/assets/icon-menu.svg'
 import cart from '@/assets/icon-cart.svg'
 import avatar from '@/assets/image-avatar.png'
 import { defineProps } from 'vue'
 import CartDropDownMenu from './CartDropDownMenu.vue'
 
-//import type { Ref } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue'
 
 defineProps<{
   isVisible: boolean;
@@ -13,6 +14,21 @@ defineProps<{
   shoppingCart: number;
   deleteProduct: () => void
 }>()
+
+const isMobile = ref(window.innerWidth < 768)
+
+const updateIsMobile = () => {
+  isMobile.value = window.innerWidth < 768
+}
+
+onMounted(() => {
+  window.addEventListener('resize', updateIsMobile)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateIsMobile)
+})
+
 </script>
 
 <template>
@@ -22,8 +38,13 @@ defineProps<{
     class="flex  flex-row justify-between border-b-2 border-[color:hsl(223, 64%, 98%)]"
     >
     <div class="flex flex-row">
-      <img class="h-10 w-auto mt-12" v-bind:src="logo" />
-      <div class="flex items-center">
+
+      <img v-if="isMobile" class="h-4 w-auto m-6" v-bind:src="menu" />
+
+
+      <img class="h-4 md:h-10 w-auto mt-6 md:mt-12" v-bind:src="logo" />
+
+      <div v-if="!isMobile" class="flex items-center">
         <div class="flex space-x-0">
           <a
           href="index.html"
@@ -60,7 +81,7 @@ defineProps<{
         </button>
         <CartDropDownMenu :deleteProduct="deleteProduct" :isVisible="isVisible" :shoppingCart="shoppingCart"/>
 
-        <img class="h-12 w-auto " v-bind:src="avatar" />
+        <img class="h-6 md:h-12 w-auto " v-bind:src="avatar" />
       </div>
     </nav>
   </div>
